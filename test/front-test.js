fbce69e283f6d2d87dd4e467b3ff04673df7b165
bertolo1988 <tiagobertolo@gmail.com>
2016-09-14 16:26:53 +0100

    isolate dom constants

--- test/front-test.js
+++ test/front-test.js
@@ -9,44 +9,64 @@ var should = require('should');
 var mongoClient = require('mongodb').MongoClient;
 var driver;
 
-function loadDemoPage() {
-    driver.get('localhost:' + config.PORT + '/demo.html');
-    let logElement = driver.findElement(By.id('log'));
-    driver.wait(Until.elementTextIs(logElement, 'Ready'), config.TIMEOUT, 'Could not locate "Ready"');
-}
-
 test.describe('Front end tests', function() {
-    this.timeout(config.TIMEOUT);
 
-    test.before(function() {
+    function loadDemoPage() {
+        driver.get('localhost:' + config.PORT + '/demo.html');
+        let logElement = driver.findElement(By.id(ID.log));
+        driver.wait(Until.elementTextIs(logElement, 'Ready'), config.TIMEOUT, 'Could not locate "Ready"');
+    }
+
+    this.timeout(config.TIMEOUT);
+    test.before(() => {
         chromedriver.start();
         driver = new Webdriver.Builder().withCapabilities(Webdriver.Capabilities.chrome()).build();
         BrowserPhoneServer.run(config);
     });
-
-    test.beforeEach(loadDemoPage);
-
-    test.after(function() {
+    test.after(() => {
         driver.quit();
         chromedriver.stop();
         BrowserPhoneServer.close();
     });
+    test.beforeEach(loadDemoPage);
+
+    const ID = {
+        number: 'number',
+        log: 'log',
+        addContact: {
+            name: 'addContactNameInput',
+            twilioId: 'addContactTwilioIdInput',
+            number: 'addContactNumberInput',
+            return: 'addContactReturnButton',
+            save: 'addContactSaveButton'
+        }
+    };
+
+    const CSS = {
+        call: 'button.call',
+        hangup: 'button.hangup',
+        addContact: 'button.browser-phone-add-contact-button',
+        addContactIcon: 'i.fa-plus',
+        'mic': 'i.fa-microphone',
+        'micSlash': 'i.fa-microphone-slash',
+        firstSuggestion: '.tt-dataset-contacts > .tt-suggestion > .tt-highlight'
+    };
 
     test.describe('Static phone-browser tests', function() {
 
         test.it('should find all components', function(done) {
-            driver.findElement(By.css('button.call')).isDisplayed();
-            driver.findElement(By.css('button.hangup')).isDisplayed();
-            driver.findElement(By.id('number')).isDisplayed();
-            driver.findElement(By.css('button.browser-phone-add-contact-button')).isDisplayed();
-            driver.findElement(By.id('log')).getText().then((text) => {
+            driver.findElement(By.css(CSS.call)).isDisplayed();
+            driver.findElement(By.css(CSS.hangup)).isDisplayed();
+            driver.findElement(By.id(ID.number)).isDisplayed();
+            driver.findElement(By.css(CSS.addContact)).isDisplayed();
+            driver.findElement(By.id(ID.log)).getText().then((text) => {
                 text.should.be.equal('Ready');
                 done();
             });
         });
 
         test.it('should see a working phone number input', function(done) {
-            let input = driver.findElement(By.id('number'));
+            let input = driver.findElement(By.id(ID.number));
             input.getAttribute('placeholder').then(function(placeholder) {
                 placeholder.should.equal('Enter a phone number...');
                 input.sendKeys('A');
@@ -62,9 +82,9 @@ test.describe('Front end tests', function() {
         });
 
         test.it('should see a disabled attribute in the hangup button, not in the call one', function(done) {
-            driver.findElement(By.css('button.hangup')).getAttribute('disabled').then(function(value) {
-                value.should.be.equal("true");
-                driver.findElement(By.css('button.call')).getAttribute('disabled').then(function(value) {
+            driver.findElement(By.css(CSS.hangup)).getAttribute('disabled').then(function(value) {
+                value.should.be.equal('true');
+                driver.findElement(By.css(CSS.call)).getAttribute('disabled').then(function(value) {
                     should.not.exist(value);
                     done();
                 });
@@ -72,9 +92,9 @@ test.describe('Front end tests', function() {
         });
 
         test.it('should see font awesome icons', function(done) {
-            driver.findElement(By.css('i.fa-microphone')).isDisplayed();
-            driver.findElement(By.css('i.fa-microphone-slash')).isDisplayed();
-            driver.findElement(By.css('i.fa-plus')).isDisplayed();
+            driver.findElement(By.css(CSS.mic)).isDisplayed();
+            driver.findElement(By.css(CSS.micSlash)).isDisplayed();
+            driver.findElement(By.css(CSS.addContactIcon)).isDisplayed();
             done();
         });
 
@@ -99,13 +119,13 @@ test.describe('Front end tests', function() {
 
         function addNewContact(user) {
             driver.findElement(By.css('.browser-phone-add-contact-button')).click();
-            let nameElement = driver.findElement(By.id('addContactNameInput'));
-            let twilioIdElement = driver.findElement(By.id('addContactTwilioIdInput'));
-            let numberElement = driver.findElement(By.id('addContactNumberInput'));
+            let nameElement = driver.findElement(By.id(ID.addContact.name));
+            let twilioIdElement = driver.findElement(By.id(ID.addContact.twilioId));
+            let numberElement = driver.findElement(By.id(ID.addContact.number));
             nameElement.sendKeys(user.name);
             twilioIdElement.sendKeys(user.twilioId);
             numberElement.sendKeys(user.number);
-            driver.findElement(By.id('addContactSaveButton')).click();
+            driver.findElement(By.id(ID.addContact.save)).click();
             driver.switchTo().alert().dismiss();
         }
 
@@ -126,21 +146,20 @@ test.describe('Front end tests', function() {
                 number: '123123123123'
             };
             addNewContact(user);
-            let firstSuggestionSelector = '.tt-dataset-contacts > .tt-suggestion > .tt-highlight';
-            let input = driver.findElement(By.id('number'));
+            let input = driver.findElement(By.id(ID.number));
             input.clear();
             input.sendKeys(user.name);
-            driver.wait(Until.elementLocated(By.css(firstSuggestionSelector)), config.TIMEOUT, 'Could not see the suggestion');
-            driver.findElement(By.css(firstSuggestionSelector)).getText().then(function(text) {
+            driver.wait(Until.elementLocated(By.css(CSS.firstSuggestion)), config.TIMEOUT, 'Could not see the suggestion');
+            driver.findElement(By.css(CSS.firstSuggestion)).getText().then(function(text) {
                 text.should.be.equal(user.name);
                 removeContact(user.name, done);
             });
         });
 
         test.it('should to use return button from add contact form', function(done) {
-            driver.findElement(By.css('.browser-phone-add-contact-button')).click();
-            driver.findElement(By.id('addContactReturnButton')).click();
-            driver.wait(Until.elementLocated(By.css('input#number')), config.TIMEOUT, 'Could not see number input!');
+            driver.findElement(By.css(CSS.addContact)).click();
+            driver.findElement(By.id(ID.addContact.return)).click();
+            driver.wait(Until.elementLocated(By.id(ID.number)), config.TIMEOUT, 'Could not see number input!');
             done();
         });
 
@@ -150,9 +169,9 @@ test.describe('Front end tests', function() {
 
         //test disabled due to http://stackoverflow.com/questions/38832776/how-do-i-allow-chrome-to-use-my-microphone-programatically
         test.xit('should be able to call a number even if not valid', function(done) {
-            let log = driver.findElement(By.id('log'));
-            driver.findElement(By.id('number')).sendKeys('123');
-            driver.findElement(By.css('button.call')).click();
+            let log = driver.findElement(By.id(ID.log));
+            driver.findElement(By.id(ID.number)).sendKeys('123');
+            driver.findElement(By.css(CSS.call)).click();
             driver.wait(Until.elementTextIs(log, 'Successfully established call'), config.TIMEOUT, 'Could not successfuly establish a call').then(function() {
                 driver.findElement(By.css('button.hangup')).click();
                 driver.wait(Until.elementTextIs(log, 'Call ended'), config.TIMEOUT, 'Could not successfuly establish a call').then(function() {
